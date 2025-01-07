ddeeba12b34878166e1ede8684114c75e31834b0
terivan <terivan2006@yandex.ru>
2025-01-06 22:17:05 -0800

    Added constants, circle shape, etc.

--- main.py
+++ main.py
@@ -3,21 +3,32 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import Player
+
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
 
     if ('SCREEN_WIDTH' not in globals()) or ('SCREEN_HEIGHT' not in globals()):
         raise pygame.error("Screen width or heigt undefined")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x,y)
 
     while True:
         screen.fill(color='black')
+        player.draw(screen)
         pygame.display.flip()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60)/1000
+
 
 
 
